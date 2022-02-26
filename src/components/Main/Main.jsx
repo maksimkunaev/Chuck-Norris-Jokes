352d67875a8ea3b3be2a6437a4514483f8d24e12
Maxim Kunaev <maksim.kunaev@westarp.io>
2022-02-26 11:52:00 +0100

    style: refactor css

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,61 +3,63 @@ import container from "../container";
 import styles from './Main.styl';
 
 const statusMessage = {
-    fetching: 'Wait...',
-    error: 'Error...',
+  fetching: 'Wait...',
+  error: 'Error...',
 };
 
 function WaitingBlock(props) {
-    const { status } = props;
-    const isVisible = status === 'fetching' || status === 'error';
-    const text = statusMessage[status];
+  const { status } = props;
+  const isVisible = status === 'fetching' || status === 'error';
+  const text = statusMessage[status];
 
-    return <div
-        className={styles.waiting}
-        style={{display: isVisible ? 'block' : 'none'}}
-    >
-        {text}
-    </div>
+  return <div
+      className={styles.waiting}
+      style={{display: isVisible ? 'block' : 'none'}}>
+      {text}
+  </div>
 }
 class Main extends PureComponent {
-    componentDidMount() {
-        this.props.setList();
-        this.scrollBottom();
+  componentDidMount() {
+    this.props.setList();
+    this.scrollBottom();
+  }
+
+  addJoke = () => {
+    const { addJoke } = this.props;
+    addJoke();
+  };
+
+  componentDidUpdate() {
+    const { status } = this.props;
+
+    if (status === 'success' || status === 'error' || status === 'fetching') {
+      this.scrollBottom();
     }
+  }
 
-    addJoke = () => {
-        const { addJoke } = this.props;
-        addJoke();
-    };
+  scrollBottom = () => {
+    const { wrap } = this;
 
-    componentDidUpdate() {
-        const { status } = this.props;
-        if (status === 'success' || status === 'error' || status === 'fetching') {
-            this.scrollBottom();
-        }
+    if (wrap) {
+        setTimeout(() => {
+            wrap.scrollTo(0,Math.pow(10,10));
+        })
     }
+  };
 
-    scrollBottom = () => {
-        const { wrap } = this;
-        if (wrap) {
-            setTimeout(() => {
-                wrap.scrollTo(0,Math.pow(10,10));
-            })
-        }
-    };
-
-    render() {
-        const { data: { list }, status } = this.props;
-        return (
-            <div className={styles.wrap} ref={node => this.wrap = node}>
-                <div className={styles.content}>
-                    <button type="button" className={styles.more} onClick={this.addJoke}>More!!!!</button>
-                    {list.map((data, index)=><div className={styles.block} key={data.id + index}>{data.value}</div>)}
-                    <WaitingBlock status={status}/>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const { data: { list }, status } = this.props;
+    
+    return (
+      <div className={styles.wrap} ref={node => this.wrap = node}>
+          <div className={styles.content}>
+              <button type="button" className={styles.more} onClick={this.addJoke}>More!!!!</button>
+              {list.map((data, index)=><div className={styles.block} key={data.id + index}>{data.value}</div>)}
+              <WaitingBlock status={status}/>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default container(Main);
